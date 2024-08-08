6382263dfb89ae65039800ad67c5572a0d4fb226
Matheus Eduardo <matheus.eduardo@itpower.co>
2024-08-08 11:32:02 -0300

    ajuste de rotas para crud

--- resources/views/Users/update.blade.php
+++ resources/views/Users/update.blade.php
@@ -3,73 +3,83 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário Reals</title>
+    <title>Editar Usuário</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
-        <form id="formUpdate" name="formUpdate">
+        <form id="formUpdate" name="formUpdate" method="POST" action="{{ route('users.update', $user->id) }}">
+            @csrf
+            @method('PUT')
             <div class="row">
                 <div class="col-md-2"></div>
                 <div class="col-md-8">
                     <div class="row">
                         <div class="col-md-2"></div>
                         <div class="col-md-8">
-                            <h1>Edição de Usuários</h1>
+                            <h1>Edição de Usuário</h1>
                         </div>
                         <div class="col-md-2"></div>
                     </div>
                     <div class="row">
                         <div class="col-md-6">
                             <label for="nome" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome">
+                            <input type="text" class="form-control" id="nome" name="name" value="{{ old('name', $user->name) }}" placeholder="Digite seu nome">
                         </div>
                         <div class="col-md-6">
                             <label for="cpf" class="form-label">CPF</label>
-                            <input type="text" class="form-control" id="cpf" name="cpf" placeholder="Digite seu CPF">
+                            <input type="text" class="form-control" id="cpf" name="cpf" value="{{ old('cpf', $user->cpf) }}" placeholder="Digite seu CPF">
                         </div>
                     </div>
                     <br>
                     <div class="row">
                         <div class="col-md-3">
                             <label for="data-nascimento" class="form-label">Data de Nascimento</label>
-                            <input type="date" class="form-control" id="data-nascimento" name="data-nascimento">
+                            <input type="date" class="form-control" id="data-nascimento" name="birthdate" value="{{ old('birthdate', $user->birthdate) }}">
                         </div>
                         <div class="col-md-9">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
+                            <input type="email" class="form-control" id="email" name="email" value="{{ old('email', $user->email) }}" placeholder="Digite seu e-mail">
                         </div>
                     </div>            
                     <br>
                     <div class="row">
                         <div class="col-md-8">
                             <label for="endereco" class="form-label">Endereço</label>
-                            <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Digite seu endereço">
+                            <input type="text" class="form-control" id="endereco" name="address" value="{{ old('address', $user->address) }}" placeholder="Digite seu endereço">
                         </div>
                         <div class="col-md-4">
                             <label for="telefone" class="form-label">Telefone</label>
-                            <input type="tel" class="form-control" id="telefone" name="telefone" placeholder="Digite seu telefone">
+                            <input type="tel" class="form-control" id="telefone" name="phone" value="{{ old('phone', $user->phone) }}" placeholder="Digite seu telefone">
                         </div>
                     </div>
                     <br>
                     <div class="row">
                         <div class="col-md-6">
                             <label for="estado" class="form-label">Estado</label>
-                            <select id="estado" name="estado" class="form-control" onchange="carregarCidades()">
+                            <select id="estado" name="state" class="form-control" onchange="carregarCidades()">
                                 <option value="">Selecione um estado</option>
+                                @foreach($estados as $estado)
+                                    <option value="{{ $estado }}" {{ old('state', $user->state) == $estado ? 'selected' : '' }}>{{ $estado }}</option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="col-md-6">
                             <label for="cidade" class="form-label">Cidade</label>
-                            <select id="cidade" disabled name="cidade" class="form-control">
+                            <select id="cidade" name="city" class="form-control">
                                 <option value="">Selecione uma cidade</option>
+                                @if(old('state', $user->state))
+                                    @foreach($cidades as $cidade)
+                                        <option value="{{ $cidade }}" {{ old('city', $user->city) == $cidade ? 'selected' : '' }}>{{ $cidade }}</option>
+                                    @endforeach
+                                @endif
                             </select>
                         </div>
                     </div>
                     <br>
                     <div class="row">
                         <div class="col-md-12">
-                            <button type="button" style="width: 100%;" class="btn btn-primary">Enviar</button>
+                            <button type="submit" style="width: 100%;" class="btn btn-primary">Atualizar</button>
                         </div>
                     </div>
                 </div>
@@ -77,7 +87,7 @@
             </div>
         </form>
     </div>
-    <script src="script.js"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-mask-plugin@1.14.16/dist/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\UserController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/cidades/{estado}', [UserController::class, 'getCidades']);
-Route::resource('users', UserController::class);
+// Rota para a listagem de usuários
 Route::get('/users', [UserController::class, 'read'])->name('users.read');
+
+// Rota para exibir o formulário de criação de um novo usuário
+Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+
+// Rota para armazenar um novo usuário no banco de dados
+Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+// Rota para exibir o formulário de edição de um usuário específico
+Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+
+// Rota para atualizar os dados de um usuário específico
+Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+
+// Rota para deletar um usuário específico
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+
+// Rota para carregar cidades dinamicamente usando a API do IBGE
+Route::get('/cidades/{estado}', [UserController::class, 'getCidades']);
+
