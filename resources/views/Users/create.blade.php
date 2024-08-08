f41946cc8cf8f01721f91668a3ed129302f83363
Matheus Eduardo <matheus.eduardo@itpower.co>
2024-08-08 11:54:10 -0300

    ajuste create

--- resources/views/Users/create.blade.php
+++ resources/views/Users/create.blade.php
@@ -8,7 +8,8 @@
 </head>
 <body>
     <div class="container">
-        <form id="formCreate" name="formCreate">
+        <form id="formCreate" name="formCreate" method="POST" action="{{ route('users.store') }}">
+            @csrf
             <div class="row">
                 <div class="col-md-2"></div>
                 <div class="col-md-8">
@@ -22,46 +23,49 @@
                     <div class="row">
                         <div class="col-md-6">
                             <label for="nome" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome">
+                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome" required>
                         </div>
                         <div class="col-md-6">
                             <label for="cpf" class="form-label">CPF</label>
-                            <input type="text" class="form-control" id="cpf" name="cpf" placeholder="Digite seu CPF">
+                            <input type="text" class="form-control" id="cpf" name="cpf" placeholder="Digite seu CPF" required>
                         </div>
                     </div>
                     <br>
                     <div class="row">
                         <div class="col-md-3">
                             <label for="data-nascimento" class="form-label">Data de Nascimento</label>
-                            <input type="date" class="form-control" id="data-nascimento" name="data-nascimento">
+                            <input type="date" class="form-control" id="data-nascimento" name="data_nascimento" required>
                         </div>
                         <div class="col-md-9">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu e-mail" required>
                         </div>
                     </div>            
                     <br>
                     <div class="row">
                         <div class="col-md-8">
                             <label for="endereco" class="form-label">Endereço</label>
-                            <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Digite seu endereço">
+                            <input type="text" class="form-control" id="endereco" name="endereco" placeholder="Digite seu endereço" required>
                         </div>
                         <div class="col-md-4">
                             <label for="telefone" class="form-label">Telefone</label>
-                            <input type="tel" class="form-control" id="telefone" name="telefone" placeholder="Digite seu telefone">
+                            <input type="tel" class="form-control" id="telefone" name="telefone" placeholder="Digite seu telefone" required>
                         </div>
                     </div>
                     <br>
                     <div class="row">
                         <div class="col-md-6">
                             <label for="estado" class="form-label">Estado</label>
-                            <select id="estado" name="estado" class="form-control" onchange="carregarCidades()">
+                            <select id="estado" name="estado" class="form-control" onchange="carregarCidades()" required>
                                 <option value="">Selecione um estado</option>
+                                @foreach($estados as $estado)
+                                    <option value="{{ $estado['sigla'] }}">{{ $estado['nome'] }}</option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="col-md-6">
                             <label for="cidade" class="form-label">Cidade</label>
-                            <select id="cidade" disabled name="cidade" class="form-control">
+                            <select id="cidade" disabled name="cidade" class="form-control" required>
                                 <option value="">Selecione uma cidade</option>
                             </select>
                         </div>
@@ -69,7 +73,7 @@
                     <br>
                     <div class="row">
                         <div class="col-md-12">
-                            <button type="button" style="width: 100%;" class="btn btn-primary">Enviar</button>
+                            <button type="submit" style="width: 100%;" class="btn btn-primary">Enviar</button>
                         </div>
                     </div>
                 </div>
@@ -77,7 +81,7 @@
             </div>
         </form>
     </div>
-    <script src="script.js"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-mask-plugin@1.14.16/dist/jquery.mask.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
